cd11e011cd790c69cae7dcf34d8a60a36c85f33e
Gerard Juan <gerardandrejuan@gmail.com>
2023-04-22 19:28:26 -0400

    delete comment blocks of outdated code

--- js/firebaseConfig.js
+++ js/firebaseConfig.js
@@ -1,35 +1,20 @@
-  // Import the functions you need from the SDKs you need
+// Import the functions you need from the SDKs you need
+import {initializeApp} from 'https://www.gstatic.com/firebasejs/9.20.0/firebase-app.js';
 
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.20.0/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+// Your web app's Firebase configuration
 
-  // TODO: Add SDKs for Firebase products that you want to use
+const firebaseConfig = {
+  apiKey: 'AIzaSyDvS5HhFa5Aice3rWu_msbHRgXhMby1XIY',
+  authDomain: 'juno-project-2.firebaseapp.com',
+  databaseURL: 'https://juno-project-2-default-rtdb.firebaseio.com',
+  projectId: 'juno-project-2',
+  storageBucket: 'juno-project-2.appspot.com',
+  messagingSenderId: '117598051344',
+  appId: '1:117598051344:web:49198e86f86009ca79fe95',
+};
 
-  // https://firebase.google.com/docs/web/setup#available-libraries
-
-
-  // Your web app's Firebase configuration
-
-  const firebaseConfig = {
-
-    apiKey: "AIzaSyDgOETC8qCBI1n5tT4JicvA7c6U71ZK6qc",
-
-    authDomain: "project-2-pronia.firebaseapp.com",
-
-    databaseURL: "https://project-2-pronia-default-rtdb.firebaseio.com",
-
-    projectId: "project-2-pronia",
-
-    storageBucket: "project-2-pronia.appspot.com",
-
-    messagingSenderId: "285950535717",
-
-    appId: "1:285950535717:web:c9840bbadd5ecdb555f7cc"
-
-  };
-
-
-  // Initialize Firebase
-
-  const app = initializeApp(firebaseConfig);
-
-  export default app
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
